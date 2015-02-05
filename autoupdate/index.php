2feb567f813c6cd9cfdcea67f471ace665d92939
Chance Hudson <jchancehud@gmail.com>
2015-02-05 12:57:48 -0600

    v1 of autoupdate

--- autoupdate/index.php
+++ autoupdate/index.php
@@ -5,10 +5,29 @@
 
 $zipURL = "https://github.com/JChanceHud/UTDCoursePlanner/archive/master.zip";
 file_put_contents("tmp.zip", fopen($zipURL, 'r'));
-//$file = new ZipArchive;
-//if($file->open("tmp.zip") === TRUE){
-	
-//}
+mkdir("new");
 
+$file = new ZipArchive;
+if($file->open("tmp.zip") === TRUE){
+	$file->extractTo('new/');
+	$file->close();
+}
+else
+	echo "Failed to open zip";
+
+$files = scandir("new");
+$source = "new/";
+$destination = "../";
+// Cycle through all source files
+foreach ($files as $file) {
+	if (in_array($file, array(".",".."))) continue;
+	if (is_dir($file)) continue;
+	// If we copied this successfully, mark it for deletion
+	copy($source.$file, $destination.$file);
+}
+
+echo "Cleaning up";
+unlink("tmp.zip");
+rmdir("new");
 
 ?>
