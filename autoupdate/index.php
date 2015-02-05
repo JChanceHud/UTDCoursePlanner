9a01757497641a464613452efd3241edc805953d
Chance Hudson <jchancehud@gmail.com>
2015-02-05 13:04:11 -0600

    autoupdate v2 - successfully works

--- autoupdate/index.php
+++ autoupdate/index.php
@@ -4,30 +4,46 @@
 //
 
 $zipURL = "https://github.com/JChanceHud/UTDCoursePlanner/archive/master.zip";
+$dirName = "UTDCoursePlanner-master";
+
 file_put_contents("tmp.zip", fopen($zipURL, 'r'));
-mkdir("new");
 
 $file = new ZipArchive;
 if($file->open("tmp.zip") === TRUE){
-	$file->extractTo('new/');
+	$file->extractTo('.');
 	$file->close();
 }
 else
 	echo "Failed to open zip";
 
-$files = scandir("new");
-$source = "new/";
+$files = scandir($dirName);
+$source = $dirName."/";
 $destination = "../";
 // Cycle through all source files
 foreach ($files as $file) {
 	if (in_array($file, array(".",".."))) continue;
-	if (is_dir($file)) continue;
+	if (is_dir($source.$file)) continue;
 	// If we copied this successfully, mark it for deletion
 	copy($source.$file, $destination.$file);
 }
 
 echo "Cleaning up";
 unlink("tmp.zip");
-rmdir("new");
+rrmdir($dirName);
+
+
+function rrmdir($dir) {
+	   if (is_dir($dir)) {
+		        $objects = scandir($dir);
+				     foreach ($objects as $object) {
+						        if ($object != "." && $object != "..") {
+									         if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+											        }
+								     }
+				     reset($objects);
+				     rmdir($dir);
+					    }
+	    }
+
 
 ?>
