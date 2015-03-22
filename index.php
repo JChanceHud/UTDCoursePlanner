bcf9c57375ed8e33bd178709eafab95336cee7eb
Chance Hudson <jchancehud@gmail.com>
2015-03-21 23:45:55 -0500

    Updated index version number

--- index.php
+++ index.php
@@ -354,7 +354,7 @@ Class distribution<br />
 
 <div class="footer">
 <div class="footertext">
-Current version: 1.2.2 | Copyright © 2015 Chance Hudson <br /> <a href="https://github.com/JChanceHud/UTDCoursePlanner">This site is open source!</a>
+Current version: 1.2.3 | Copyright © 2015 Chance Hudson <br /> <a href="https://github.com/JChanceHud/UTDCoursePlanner">This site is open source!</a>
 </div>
 </div>
 
