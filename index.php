f21435283783cc80bf1a65adb62158565f747871
Chance Hudson <jchancehud@gmail.com>
2015-03-23 21:36:45 -0500

    Remove unused include

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-<script src="html2canvas.js"></script>
 
 <script src="coursescheduler.js"></script>
 </head>
